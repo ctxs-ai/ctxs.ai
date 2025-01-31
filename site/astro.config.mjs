7c7accd3fde5a30852d5f3fab25a26e4efa426e9
Martin Klepsch <martinklepsch@googlemail.com>
2025-01-31 18:56:20 +0200

    add cloudflare adpater

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ctxs.ai',
   output: 'static',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: cloudflare()
 });
