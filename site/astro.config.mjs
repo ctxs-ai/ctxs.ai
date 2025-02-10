6d9895b5445c754ae557152c4b6b160d04ea2962
Martin Klepsch <martinklepsch@googlemail.com>
2025-02-10 09:47:52 +0200

    tailwind

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
+import tailwindcss from "@tailwindcss/vite";
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ctxs.ai',
   output: 'static',
-  integrations: [tailwind()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
