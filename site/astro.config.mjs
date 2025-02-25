c2f033f328e4e8313b8c486785bb8278fe329f08
Martin Klepsch <martinklepsch@googlemail.com>
2025-02-25 12:49:41 +0200

    switch back to static

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ctxs.ai',
-  output: 'server',
+  output: 'static',
   adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
