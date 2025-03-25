8d99c47a5ff843ad29335bb25f33533c9e74dea2
Martin Klepsch <martinklepsch@googlemail.com>
2025-03-25 13:42:55 +0200

    fix frontmatter stuff

--- site/src/lib/utils.ts
+++ site/src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import yaml from 'js-yaml';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
