6af1c8b5b03cf4f151909fee7ecab0adc7d24211
Martin Klepsch <martinklepsch@googlemail.com>
2025-04-15 17:09:43 +0200

    xx

--- apps/web/src/lib/db.ts
+++ apps/web/src/lib/db.ts
@@ -1,21 +1,26 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "@ctxs/db";
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+// Consider using this for serverless deployments
+// import { drizzle } from 'drizzle-orm/neon-http';
+// import { neon } from '@neondatabase/serverless';
+
+import * as schema from '@ctxs/db';
 
 // Create a PostgreSQL client
 export let db;
 
 const connectionString = process.env.DATABASE_URL!;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   // Local development connection
-  console.log("[DEV] initializing PostgreSQL db", connectionString);
+  console.log('[DEV] initializing PostgreSQL db', connectionString);
   const client = postgres(connectionString);
   db = drizzle(client, { schema });
 } else {
-  console.log("[PROD] initializing PostgreSQL db");
+  console.log('[PROD] initializing PostgreSQL db');
   if (!connectionString) {
-    throw new Error("DATABASE_URL is not defined in production environment");
+    throw new Error('DATABASE_URL is not defined in production environment');
   }
   const client = postgres(connectionString);
   db = drizzle(client, { schema });
